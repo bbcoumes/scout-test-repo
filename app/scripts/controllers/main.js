cb2587922b76eacc22b365e3b3cb16cb3a98ec32
Ben <bbcoumes@gmail.com>
2015-10-29 21:35:40 -0400

    Initial Commitz

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scoutTestRepoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the scoutTestRepoApp
+ */
+angular.module('scoutTestRepoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
