38556e415248df9a0bb44af1b0a0c308bb05be58
Ben <bbcoumes@gmail.com>
2015-11-03 22:39:24 -0500

    another test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,7 +18,7 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
+      .when('/anothertest', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
